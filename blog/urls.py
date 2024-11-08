52ee98f21b79fcbea6080fcac942bae37c1522d6
Dauren <qabyl.da2@altynbank.kz>
2024-11-08 12:00:01 +0500

    create post form

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.PostListView.as_view(), name='get_posts'),
     path('<int:pk>/', views.PostDetailView.as_view(), name='get_post_details'),
     path('author/<str:author>/', views.PostsByAuthorView.as_view(), name='posts_by_author'),
+    path('create/', views.create_post, name='create_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView
 
+from .forms import PostForm
 from .models import Post, Comment
 
 
@@ -13,6 +14,17 @@ def get_post_details(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     comments = Comment.objects.filter(post=post)
     return render(request, "post_details.html", {"post": post, "comments": comments})
+
+
+def create_post(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(get_posts)
+    else:
+        form = PostForm()
+    return render(request, "post_form.html", {"form": form})
 
 
 class PostListView(ListView):
